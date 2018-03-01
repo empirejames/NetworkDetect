8723b351e757950c516e75d52d11f1e2597690d7
james <empirejames@hotmail.com>
2018-03-01 11:12:40 +0800

    Fix ios select has no Done button to select working normally

--- www/js/app.js
+++ www/js/app.js
@@ -199,11 +199,11 @@ angular.module('starter', ['ionic','ngCordova'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
+    
     if(window.cordova && window.cordova.plugins.Keyboard) {
       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
       // for form inputs)
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(false);
-
       // Don't remove this line unless you know what you are doing. It stops the viewport
       // from snapping when text inputs are focused. Ionic handles this internally for
       // a much nicer keyboard experience.
